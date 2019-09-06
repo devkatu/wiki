0c81c87f9704b994e6489fb632ef58016fcb5600
katu1001 <katu1001@mineo.jp>
2019-09-06 17:06:08 +0900

    ajax追加したけど なんかうまくいかない

---
+++ ajax.php
@@ -0,0 +1,17 @@
+<?php
+echo htmlspecialchars($_GET['txt'] . " との入力を受け付けました" , ENT_QUOTES , "utf-8");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge"> -->
+  <title>Document</title>
+</head>
+<body>
+  <div class="box">
+    <p>message</p>
+  </div>
+</body>
+</html>
