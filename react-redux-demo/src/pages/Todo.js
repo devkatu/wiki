4f5a8d22e2e23fec5f7d35bfc557db5cbb9c7f61
katu1001 <katu1001@mineo.jp>
2020-12-17 13:35:05 +0900

    少し前進

--- react-redux-demo/src/pages/Todo.js
+++ react-redux-demo/src/pages/Todo.js
@@ -1,12 +1,14 @@
 
 import { AppBar, Button, Checkbox, Container, IconButton, List, ListItem, ListItemIcon, ListItemSecondaryAction, ListItemText, MenuItem, Select, TextField, Toolbar, Typography } from "@material-ui/core";
-import MenuIcon  from "@material-ui/icons/Menu";
+import MenuIcon from "@material-ui/icons/Menu";
 import { makeStyles } from "@material-ui/styles";
 import { push } from "connected-react-router";
 import { useDispatch, useSelector } from "react-redux";
 
 import Header from '../components/Header';
 import DrawerMenu from "../components/DrawerMenu";
+import { TodayOutlined } from "@material-ui/icons";
+import { useState } from "react";
 
 const useStyles = makeStyles({
 
@@ -15,59 +17,52 @@ const useStyles = makeStyles({
 const Todo = () => {
   const dispatch = useDispatch();
   const classes = useStyles();
-  const st = useSelector(state => state);
+  const [inputText, setInputText] = useState("");
+  const todos = useSelector(state => state.todos.entities);
+
+  const inputTextHandler = (e) => {
+    setInputText(e.target.value);
+  }
   return (
     <>
-      <Header/>
+      <Header />
       <Container maxWidth="sm">
-        <TextField placeholder="やることを入力してね" label="TODO" fullWidth/>
-        <button onClick={() => {dispatch({type: "TEST",payload: "payload test"})}}>test action</button>
-        <p>{st.todos.todos.todo}</p>
+        <TextField placeholder="やることを入力してね" label="TODO" onChange={inputTextHandler}/>
+        <Button
+        variant="contained"
+          onClick={() => { dispatch({
+            type: "TODOS/TODO_ADDED",
+            payload: {
+              text: inputText
+            }
+          }) }}>
+          Todoを登録する
+        </Button>
         <List>
-          <ListItem>
-            <ListItemIcon>
-            <Checkbox
-              edge="start"
-              // checked={}
-              disableRipple
-            />
-            </ListItemIcon>
-            <ListItemText primary={"aaa"}/>
-            <ListItemSecondaryAction>
-              <Select
-                edge="end"
-                // value={}
-                // onChange={}
-              >
-                <MenuItem value={"green"}>green</MenuItem>
-                <MenuItem value={"red"}>red</MenuItem>
-                <MenuItem value={"blue"}>blue</MenuItem>
-                
-              </Select>
-            </ListItemSecondaryAction>
-          </ListItem>
-          <ListItem>
-            <ListItemIcon>
-            <Checkbox
-              edge="start"
-              // checked={}
-              disableRipple
-            />
-            </ListItemIcon>
-            <ListItemText primary={"aaa"}/>
-            <ListItemSecondaryAction>
-              <Select
-                edge="end"
-                // value={}
-                // onChange={}
-              >
-                <MenuItem value={"green"}>green</MenuItem>
-                <MenuItem value={"red"}>red</MenuItem>
-                <MenuItem value={"blue"}>blue</MenuItem>
-                
-              </Select>
-            </ListItemSecondaryAction>
-          </ListItem>
+          {todos.map((todo, index) =>
+            <ListItem key={index}>
+              <ListItemIcon>
+                <Checkbox
+                  edge="start"
+                  checked={todo.completed}
+                  disableRipple
+                />
+              </ListItemIcon>
+              <ListItemText primary={todo.text} />
+              <ListItemSecondaryAction>
+                <Select
+                  edge="end"
+                  value={todo.color}
+                // onChange={ }
+                >
+                  <MenuItem value={"green"}>green</MenuItem>
+                  <MenuItem value={"red"}>red</MenuItem>
+                  <MenuItem value={"blue"}>blue</MenuItem>
+
+                </Select>
+              </ListItemSecondaryAction>
+            </ListItem>
+          )}
         </List>
       </Container>
     </>
--- react-redux-demo/src/state/todo.js
+++ react-redux-demo/src/state/todo.js
@@ -1,19 +1,51 @@
 const initialState = {
-  todos: {
-    todo: "test todo",
-    complete: false,
-    color: "nothing",
+  entities: 
+    // [todo.id]: {
+      [
+        {
+          id: "1",
+          text: "test1",
+          completed: false,
+          color: "nothing",
+        },
+        {
+          id: "2",
+          text: "test2",
+          completed: false,
+          color: "green",
+        },
+        {
+          id: "3",
+          text: "test3",
+          completed: true,
+          color: "nothing",
+        },
+        
+      ]
 
-  }
-  ,
-  
-  
-  fetching: false,
-  fetched: false
+
+  // fetching: false,
+  // fetched: false
 }
 
 export const TodosReducer = (state = initialState, action) => {
   switch (action.type) {
+    case "TODOS/TODO_ADDED":
+      const todo = action.payload;
+      return {
+        ...state,
+        entities: [
+          ...state.entities,
+          {
+            id: "",
+            text: todo.text,
+            completed: false,
+            color: "nothing"
+          }
+        ]
+      }
+
+
     case "FETCH_TODO":
       return {
         ...state,
