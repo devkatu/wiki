85137b414d3f542949cf4f897cd5ebd188873de7
katu1001 <katu1001@mineo.jp>
2021-05-29 22:23:20 +0900

    コンテキストの説明追加

--- react/sample/src/pages/MyContext.js
+++ react/sample/src/pages/MyContext.js
@@ -6,6 +6,15 @@ import React from 'react';
 // ツリーの色々なところで必要なpropsを子要素の子要素へ…とひたすら書かずに
 // contextをstaticで読み取ってthis.contextにて値を受け渡すことができる。
 // ネストが深い所へ値を渡すのが楽になる
+
+// React.createContext()の引数でコンテキストの初期値を設定でき、
+// Context.Providerのvalueプロップにて任意の値を設定できる
+// クラスコンポーネントではContext.Providerで括った中身のコンポーネントで
+// static contextType に作成したコンテキストを入れると this.contextで値を読み取れる
+// 関数コンポーネントではContext.Providerで括った中身で
+// const context = useContext(作成したコンテキスト)　のcontextで値を読み取れる
+
+
 const ThemeContext = React.createContext('light');
 export default class MyContext extends React.Component {
   // static contextTypeに作成したコンテキストを読込むとthis.contextとして使えるみたい
